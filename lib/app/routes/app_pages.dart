fb1dffdd47978c862865b65e3ebf06a354749d6b
rafigustiar <rafigustiar2805@gmail.com>
2024-06-14 20:39:55 +0700

    Transaksi dan anggaran

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -49,7 +49,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.ANGGARAN;
+  static const INITIAL = Routes.HOME;
 
   static final routes = [
     GetPage(
